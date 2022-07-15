c6da07bb0a0a7990cb4aa2eed2d29707d80dbaf8
Benjamin Chan <benjamin.chan@dhsoha.state.or.us>
2022-07-15 10:05:50 -0700

    Create discharge data query

---
+++ discharges.sql
@@ -0,0 +1,15 @@
+select A.patient_last_name,
+       B.geo_census_id,
+       B.geo_state_fips,
+       B.geo_county_fips,
+       B.geo_census_tract,
+       B.geo_census_block,
+       B.geo_result_category,
+       A.race,
+       A.ethnicity,
+       A.age,
+       A.gender
+from hosp.hdd A inner join
+     hosp.hdd_geocode B on A.record_id = B.record_id
+where year(A.discharge_date) = 2021
+;
